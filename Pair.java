9815d1ea41c33a452ca107f82f2f2e508057b3fd
dunaya <dudar.av@phystech.edu>
2021-10-08 16:18:30 +0300

    Update Pair.java

--- Pair.java
+++ Pair.java
@@ -27,24 +27,51 @@ public class Pair<T, S> {
         return first;
     }
 
-    /** A getter
+    /**
+     * A getter
      * @return the value of second parameter
      */
     public S getSecond(){
         return second;
     }
 
+
+    /**
+     * Indicates whether some other object is "equal to" this Pair. The other object is considered equal if:
+     * it is also a Pair and;
+     * both instances have no value present or;
+     * the present first values are "equal to" each other via equals(), the same with second values.
+     * @param o
+     * @return
+     */
     @Override
     public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null && this == null) return true;
-        if (getClass() != o.getClass()) return false;
-        Pair<?, ?> pair = (Pair<?, ?>) o;
-        return Objects.equals(getFirst(), pair.getFirst()) && Objects.equals(getSecond(), pair.getSecond());
+        if (this == o)
+            return true;
+        if (o == null)
+            return false;
+        if (getClass() != o.getClass())
+            return false;
+        Pair pair = (Pair) o;
+        if (getFirst() == null) {
+            if (pair.getFirst() != null)
+                return false;
+        } else if (!getFirst().equals(pair.getFirst()))
+            return false;
+        if (getSecond() == null) {
+            if (pair.getSecond() != null)
+                return false;
+        } else if (!getSecond().equals(pair.getSecond()))
+            return false;
+        return true;
     }
 
+    /**
+     * @return Returns the hash code value of the present values, if any, or 0 (zero) if no value is present.
+     */
     @Override
     public int hashCode() {
+        if (getFirst() == null && getSecond() == null) return 0;
         return Objects.hash(getFirst(), getSecond());
     }
 
@@ -52,7 +79,7 @@ public class Pair<T, S> {
      * Method creates a Pair class pair out of two parameters
      * @param first
      * @param second
-     * @return
+     * @return a Pair object
      */
     public static Pair of(Object first, Object second){
         return new Pair(first, second);
@@ -60,7 +87,7 @@ public class Pair<T, S> {
 
     /**
      * Method makes a Pair class object String class object
-     * @return
+     * @return a String object
      */
     public String toString(){
         return this.first + "," + this.second;
