8cf17f96139552388f360748810e1c719caf803a
dunaya <dudar.av@phystech.edu>
2021-10-09 01:28:17 +0300

    Update Main.java

--- Numbers/Main.java
+++ Numbers/Main.java
@@ -3,34 +3,37 @@ import java.util.List;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) throws IncorrectDataException {
-        List<Integer> numbers = new ArrayList<>();
-        ArrayList<Integer> numbersThree = new ArrayList<>();
-        ArrayList<Integer> numbersTwo = new ArrayList<>();
-        ArrayList<Integer> numbersOther = new ArrayList<>();
-        Scanner in = new Scanner(System.in);
-        String numbersString = in.nextLine();
-        String[] splittedData = numbersString.split(" ");
+    public static ArrayList<Integer> parseNumbers(String args) throws IncorrectDataException {
+        String[] splittedData = args.split(" ");
+        ArrayList<Integer> list = new ArrayList<>();
         if (splittedData.length != 20) {
-            throw new IncorrectDataException("You've entered incorrect data: " + numbersString);
+            throw new IncorrectDataException("You've entered invalid data: " + args);
         } else {
             for (int i = 0; i <= 19; i++) {
                 try {
-                    numbers.add(Integer.parseInt(splittedData[i]));
-                    if (numbers.get(i) % 3 == 0) {
-                        numbersThree.add(numbers.get(i));
-                    }
-                    if (numbers.get(i) % 2 == 0) {
-                        numbersTwo.add(numbers.get(i));
-                    }
-                    if (numbers.get(i) % 3 != 0 && numbers.get(i) % 2 != 0){
-                        numbersOther.add(numbers.get(i));
-                    }
-
+                    list.add(Integer.parseInt(splittedData[i]));
                 } catch (NumberFormatException ex) {
-                    throw new IncorrectDataException(" You've entered incorrect data:" + numbersString);
+                    throw new IncorrectDataException(" You've entered invalid data:" + args);
                 }
             }
+        }
+        return list;
+    }
+    public static void main(String[] args) throws IncorrectDataException {
+        ArrayList<Integer> numbersThree = new ArrayList<>();
+        ArrayList<Integer> numbersTwo = new ArrayList<>();
+        ArrayList<Integer> numbersOther = new ArrayList<>();
+        String line = (new Scanner(System.in)).nextLine();
+        ArrayList<Integer> listMain = parseNumbers(line);
+        for (int i = 0; i <= 19; i++) {
+            if (listMain.get(i) % 3 == 0){
+                numbersThree.add(listMain.get(i));
+            }
+            if (listMain.get(i) % 2 == 0){
+                numbersTwo.add(listMain.get(i));
+                continue;
+            }
+            numbersOther.add(listMain.get(i));
         }
         printList(numbersThree);
         printList(numbersTwo);
